991ae39ccecfa07bb8f4f5d9a7e5ef0cc8e9ecd5
YILIN <yilinyan1030@gmail.com>
2018-10-14 11:17:20 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 function setup() {
+/*
     var canvas = createCanvas(700, 500);
     canvas.parent("canvas");
     background (0);
+    */
 }
 
 function draw() {
+    /*
     if (mouseIsPressed) {
       fill(0);
     } else {
       fill(255);
     }
     ellipse(mouseX, mouseY, 80, 80);
+    */
 }
